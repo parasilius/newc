fb588ff5c7b25a4400d3d7c34b6c27d4cc5a1034
Parsa Hosseini <parsahosseini2001@gmail.com>
2022-08-30 18:11:52 +0430

    Add function

---
+++ newc.sh
@@ -0,0 +1,1 @@
+newc() { echo "#include <stdio.h>" >> test.c && echo "" >> test.c && echo "int main(void)" >> test.c && echo "{" >> test.c && echo "\x20\x20\x20\x20return 0;" >> test.c && echo "}" >> test.c }
